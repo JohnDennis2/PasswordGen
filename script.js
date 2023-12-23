10262dc9bac3f3c3f3fe53f95585ab1b66e6445a
John D <johnd@Johns-MBP.attlocal.net>
2023-12-22 23:36:42 -0600

    some more

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // Assignment code here
-const numbers = "1,2,3,4,5,6,7,8,9"
-const special =  "~`! @#$%^&*()-_+={}[]|\;:"<>,./?""
-const upperLetters = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
-const lowerLetters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+
 
 
 
@@ -14,32 +11,51 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 
+
+const numbers = "1,2,3,4,5,6,7,8,9"
+const special =  "~`! @#$%^&*()-_+={}[]|;:"
+const upperLetters = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
+const lowerLetters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+
+
+
+let chooseSpecial = confirm("Would you like a special character?")
+let choosenumbers = confirm("Would you like to choose a number?")
+let chooseUpperLetters = confirm("Would you like an uppercase letter?")
+let chooseLowerLetters = confirm("Would you like a lowercase letter?")
+
+
 function writePassword() {
+
+
+
+
+
+Passwordlength = 8
+ Passwordlength = (i = 0; i <= 8 ; i++)
+
 
   var generateBtn = document.querySelector("#generate");
   generateBtn.addEventListener("click", writePassword);
 
-  let chooseSpecial = confirm("Would you like a special character?")
-let choosenumbers = confirm("Would you like to choose a number?")
-let chooseUpperLetters = confirm("Would you like an uppercase letter?")
-let chooseLowerLetters = confirm("Would you like a lowercase letter?")
+  
 
 
 passwordInput = Math.floor(Math.random()*Passwordlength);
 
 
 if (chooseSpecial) {
-  special.querySelector.chooseSpecial
+  choosenumbers
 }
 
 choosenumbers
 if {
-    
+    chooseUpperLetters
 }
 
 chooseUpperLetters {
 
-
+chooseLowerLetters
 
 }
 chooseLowerLetters {
@@ -47,9 +63,10 @@ chooseLowerLetters {
 
 }
 
-for( Passwordlength = (i = 0; i < 8 ; i++))
 
 
+
+passwordInput = Math.floor(Math.random()*Passwordlength);
 
   var passwordText = document.querySelector(numbers,special,upperLetters,lowerLetters);
 
@@ -62,5 +79,5 @@ for( Passwordlength = (i = 0; i < 8 ; i++))
 }
 
 // Add event listener to generate button
-document.getElementById.generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
 
