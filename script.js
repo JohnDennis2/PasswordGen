b5b640d6065f4a5716291e8547021b1cff93f976
John D <johnd@Johns-MBP.attlocal.net>
2023-12-22 19:46:09 -0600

    trying so hard 1

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Assignment code here
-
-
+const numbers = "1,2,3,4,5,6,7,8,9"
+const special = ""
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz"
+var length = 8
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(numbers,special,letters) {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
