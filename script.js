29dcc8815ae1e74691d7e78dd9e073eb16f68a09
John D <johnd@Johns-MBP.attlocal.net>
2023-12-29 15:48:53 -0600

    almost there

--- script.js
+++ script.js
@@ -22,67 +22,68 @@
 
 
 // Get references to the #generate element
-
-
-
-
-// Write password to the #password input
-
-
+var generateBtn = document.querySelector("#generate");
 
 const numbers = [1,2,3,4,5,6,7,8,9]
 const special =  [",~,`,!, @,#,$,%,^,&,*,(,)-,_,+,=,{,},[,],|,;,:,"]
 const upperLetters = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"]
 const lowerLetters = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"] 
 
-let Passwordlength = prompt("please choose a password length between 8 and 128 characters")
+
+// Write password to the #password input
+
+function writePassword() {
+
+  var password = generatePassword();
+
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
+
+function generatePassword() { 
+
+  const passWordLength = function howlong() {
+    
+        passLength = prompt("Please choose a password length")
+ 
+if { 
+    charcNum = (8 > || < 128)
+         
+
+    }
+    
+
+
+    for (i=0; i < 128 || > 8, i++)
+console.log( passWordLength)
+
+  }
 
 let chooseSpecial = confirm("Would you like a special character?",)
 let choosenumbers = confirm("Would you like to a number?",)
 let chooseUpperLetters = confirm("Would you like an uppercase letter?",)
 let chooseLowerLetters = confirm("Would you like a lowercase letter?",)
 
-let whatSpecial = prompt("please choose a special character")
-let whatNumber = prompt("please choose a number")
-let whatUpper = prompt("please choose an uppper lette")
-let whatLower = prompt("please choose a lower letter")
 
-function passChoices ()
- {
 
-    
+  
+
+  }
+  
 
 
 
-var generateBtn = document.querySelector("#generate");
-
-function writePassword() {
-
-
-
-} 
-
-Passwordlength = 8
- PasswordlengthChoice  = ('i = 0; i <= 8 ; i++')
-
-const ChoicesArray = [ {chooseLowerLetters} , {chooseUpperLetters}, {chooseSpecial}, {choosenumbers}]
- 
-
-passwordInput = Math.floor(Math.random()*Passwordlength);
-
-  var passwordText = document.querySelector(numbers,special,upperLetters,lowerLetters);
-
-  passwordText.value = password;
-
-  var password = generatePassword();
 
 return password;
 
 }
 
 // Add event listener to generate button
+
+
+}
+
 generateBtn.addEventListener("onClick", writePassword);
-
-
 generatePassword()
+writePassword()
 
